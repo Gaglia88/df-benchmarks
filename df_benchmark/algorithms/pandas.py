e49b054c88b132d3b18f0e80f675710193af4121
zecchiniluca <zecchini.luca@libero.it>
2021-11-23 15:47:14 +0100

    pandas.py

--- df_benchmark/algorithms/pandas.py
+++ df_benchmark/algorithms/pandas.py
@@ -4,7 +4,7 @@ import pandas as pd
 class pandasBench(BaseDfBench):
     def __init__(self):
         pass
-    
+
     def load_dataset(self, path, format, **kwargs):
         """
         Load the provided dataframe
@@ -14,17 +14,23 @@ class pandasBench(BaseDfBench):
         elif format == "json":
             self.df = pd.read_csv(path, **kwargs)
         pass
-        
+
+    def delete_columns(self, columns):
+        """
+        Delete the specified columns
+        """
+        return self.df.drop(columns=columns)
+
     def fill_nan(self, value):
         """
         Fill nan values in the dataframe with the provided value
         """
         return self.df.fillna(value)
         pass
-    
+
     def sort(self, columns, ascending=True):
         """
-        Sort the dataframe by the provided columns.
+        Sort the dataframe by the provided columns
         """
         return self.df.sort_values(columns, ascending=ascending)
         pass
